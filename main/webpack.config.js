0310c8b3c09863d25453df53ad6743ea2be74460
John Gilbert <1082126+jgilbert01@users.noreply.github.com>
2021-02-17 21:53:33 -0500

    clean up

--- main/webpack.config.js
+++ main/webpack.config.js
@@ -1,34 +1,54 @@
+const path = require('path');
 const { merge } = require('webpack-merge');
-const singleSpaDefaults = require("webpack-config-single-spa-react");
+const singleSpaDefaults = require('webpack-config-single-spa-react');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports = (webpackConfigEnv, argv) => {
-  const orgName = 'mfe';
+const orgName = 'mfe';
+const projectName = 'main';
+const SHA = process.env.CI_COMMIT_SHA || process.env.GITHUB_SHA || 'stg';
+
+module.exports = (webpackConfigEnv, argv) => {  
   const defaultConfig = singleSpaDefaults({
     orgName,
-    projectName: 'main',
+    projectName,
     webpackConfigEnv,
     argv,
     disableHtmlGeneration: true,
   });
 
+  const SHA = process.env.CI_COMMIT_SHA || process.env.GITHUB_SHA || 'stg';
+
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
     // externals: [/^@org-name\/.+/]    
+    output: {
+      path: path.resolve(
+        process.cwd(),
+        'dist',
+        `${orgName}-${projectName}`,
+        SHA
+      ),
+    },
     plugins: [
       new HtmlWebpackPlugin({
         inject: false,
         template: 'src/index.ejs',
+        filename: '../../index.html',
         templateParameters: {
           isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
           orgName,
         },
       }),
       new CopyPlugin({
-        patterns: [
-          { from: 'mfe.json' },
-        ],
+        patterns: [{
+          from: 'mfe.json',
+          transform: {
+            transformer(content) {
+              return Buffer.from(content.toString().replace(/SHA/g, SHA));
+            },
+          },
+        }],
       }),
     ],
   });
