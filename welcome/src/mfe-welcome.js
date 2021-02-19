76e1e91e7bb81cb1178cb0a98741cd641444f533
John Gilbert <1082126+jgilbert01@users.noreply.github.com>
2021-02-19 09:52:34 -0500

    clean up

--- welcome/src/mfe-welcome.js
+++ welcome/src/mfe-welcome.js
@@ -1,23 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import singleSpaReact from "single-spa-react";
-import Root from "./root.component";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import singleSpaReact from 'single-spa-react';
 
 const lifecycles = singleSpaReact({
   React,
   ReactDOM,
-  rootComponent: Root,
+  loadRootComponent: () => import('./root.component.js').then((mod) => mod.default),
+  domElementGetter: () => document.getElementById('container'),
   errorBoundary(err, info, props) {
     return <div>Error</div>;
   },
-
-  // errorBoundary() {
-  //   return <div>Error</div>;
-  // },
-  // loadRootComponent: () =>
-  //   import(
-  //     "./root.component.js"
-  //   ).then((mod) => mod.default),
 });
 
 export const bootstrap = lifecycles.bootstrap;
