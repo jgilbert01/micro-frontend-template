da71d368cfca3749fbdd0aef594d5368c7dc1fd1
John Gilbert <1082126+jgilbert01@users.noreply.github.com>
2021-04-11 10:35:49 -0400

    home updates

--- home/src/root.component.js
+++ home/src/root.component.js
@@ -2,8 +2,11 @@ import React from "react";
 
 export default function Root(props) {
   return (
-    <h1>
-      <p>{props.name} is mounted!</p>
-    </h1>
+    <div>
+      <h1>
+        <p>{props.name} is mounted!</p>
+      </h1>
+      <pre>{JSON.stringify(props, null, 2)}</pre>
+    </div>
   );
 }
