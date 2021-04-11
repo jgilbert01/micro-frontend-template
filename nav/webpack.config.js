1cd3dffc9f8b7052ab4b9c6f74f24bb19f8e85f9
John Gilbert <1082126+jgilbert01@users.noreply.github.com>
2021-04-11 11:25:49 -0400

    fixed nav imports

--- nav/webpack.config.js
+++ nav/webpack.config.js
@@ -1,27 +1,41 @@
-const path = require("path");
+const path = require('path');
 const { merge } = require('webpack-merge');
 const singleSpaDefaults = require('webpack-config-single-spa-react');
 const CopyPlugin = require('copy-webpack-plugin');
 
+const orgName = 'mfe';
+const projectName = 'nav';
+const SHA = process.env.CI_COMMIT_SHA || process.env.GITHUB_SHA || 'stg';
+
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
-    orgName: 'mfe',
-    projectName: 'nav',
+    orgName,
+    projectName,
     webpackConfigEnv,
     argv,
   });
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
-    // externals: [/^@org-name\/.+/]    
+    // externals: [/^@org-name\/.+/]
     output: {
-      path: path.resolve(process.cwd(), 'dist', 'mfe-nav', process.env.GITHUB_SHA || 'stg'),
+      path: path.resolve(
+        process.cwd(),
+        'dist',
+        `${orgName}-${projectName}`,
+        SHA
+      ),
     },
     plugins: [
       new CopyPlugin({
-        patterns: [
-          { from: 'mfe.json' },
-        ],
+        patterns: [{
+          from: 'mfe.json',
+          transform: {
+            transformer(content) {
+              return Buffer.from(content.toString().replace(/SHA/g, SHA));
+            },
+          },
+        }],
       }),
     ],
   });
